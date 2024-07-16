ff89cf9a473a22c7a95732a8fc5ebe30595a0765
ktkim <ktkim@aimmed.com>
2024-07-16 16:48:18 +0900

    chord

---
+++ src/configs/chords.js
@@ -0,0 +1,71 @@
+const major = [0, 16, 19];
+const minor = [0, 15, 19];
+const aug = [0, 16, 20];
+const dim = [0, 15, 18];
+const sus4 = [0, 17, 19];
+const major7 = [0, 11, 16, 19];
+const minor7 = [0, 10, 15, 19];
+const dim7 = [0, 9, 15, 18];
+const mM7 = [0, 10, 16, 19, 23];
+const b5 = [0, 16, 18];
+const mb5 = [0, 15, 18];
+const six = [0, 7, 16, 21];
+const seven = [0, 10, 16, 19];
+const nine = [0, 10, 14, 16, 19];
+const eleven = [0, 4, 10, 17, 19];
+const thirteen = [0, 10, 16, 21];
+const seven913 = [0, 10, 14, 16, 21];
+const sevenF9F13 = [0, 4, 7, 10, 13, 20];
+const sevenS9F13 = [0, 4, 7, 10, 15, 20];
+const seven9S1113 = [0, 4, 7, 10, 14, 17, 21];
+
+const add2 = [0, 14, 16, 19];
+const fNine = [0, 10, 13, 16, 19];
+const sNine = [0, 10, 15, 16, 19];
+const sEleven = [0, 4, 10, 18, 19];
+const fThirteen = [0, 10, 16, 20];
+
+export const list = [
+  major,
+  minor,
+  add2,
+  aug,
+  dim,
+  sus4,
+  minor7,
+  major7,
+  dim7,
+  mM7,
+  b5,
+  mb5,
+  six,
+  seven,
+  fNine,
+  nine,
+  sNine,
+  eleven,
+  sEleven,
+  fThirteen,
+  thirteen,
+  seven913,
+  sevenF9F13,
+  sevenS9F13,
+  seven9S1113,
+];
+
+export const noteList = [
+  "C",
+  "Db",
+  "D",
+  "Eb",
+  "E",
+  "F",
+  "Gb",
+  "G",
+  "Ab",
+  "A",
+  "Bb",
+  "B",
+];
+
+export const START_NOTE = 36;
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -3,11 +3,65 @@ import PixelWriting from "../components/PixelWriting";
 import Logo from "../components/Logo";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { list, START_NOTE } from "../configs/chords";
+import { FREQUENCIES } from "../configs/frequencies";
 
 export default function MainPage() {
   const navigate = useNavigate();
   const [haederName, setHeaderName] = useState("header");
+  const [content, setContent] = useState("main");
   const [isMemu, setIsMemu] = useState(false);
+  const FONT_SIZE = 3;
+  const HEADER_SIZE = 10;
+  const duration = 0.4;
+
+  const audioContext = new window.AudioContext();
+
+  function playNote(frequency: number, startTime: number, duration: number) {
+    const oscillator = audioContext.createOscillator();
+
+    oscillator.type = "triangle";
+    oscillator.frequency.value = frequency;
+
+    const gainNode = audioContext.createGain();
+    gainNode.gain.setValueAtTime(0.5, audioContext.currentTime);
+    gainNode.gain.exponentialRampToValueAtTime(
+      0.1,
+      audioContext.currentTime + duration
+    );
+
+    oscillator.connect(gainNode);
+    gainNode.connect(audioContext.destination);
+
+    oscillator.start(audioContext.currentTime + startTime);
+    oscillator.stop(audioContext.currentTime + startTime + duration);
+  }
+
+  function playMusic() {
+    const arr = list[0];
+    console.log(arr);
+    const arr2 = arr.map((note) => FREQUENCIES[note + START_NOTE]);
+    console.log(arr2);
+    const notes = [{ freq: arr2, time: 0, duration: duration * 2 }];
+
+    // const notes = [
+    //   { freq: 261.63, time: 0, duration }, // C4
+    //   { freq: 329.63, time: 0.5, duration }, // E4
+    //   { freq: 392.0, time: 1, duration }, // G4
+    //   { freq: 523.25, time: 1.5, duration }, // C5
+    //   { freq: [261.63, 329.63, 392.0], time: 2, duration: duration * 2 }, // C Major Chord
+    //   { freq: 392.0, time: 3, duration }, // G4
+    //   { freq: 329.63, time: 3.5, duration }, // E4
+    // ];
+
+    notes.forEach((note) => {
+      if (Array.isArray(note.freq)) {
+        note.freq.forEach((f) => playNote(f, note.time, note.duration));
+      } else {
+        playNote(note.freq, note.time, note.duration);
+      }
+    });
+  }
 
   return (
     <div className={styles.container}>
@@ -20,26 +74,64 @@ export default function MainPage() {
         >
           <Logo multi={10} />
         </div>
-        <div className={styles.pixelWriting}>
+        <div className={styles.headerText}>
           <PixelWriting
             str={haederName}
-            fontProps={{ size: 10, color: "#f0fff0" }}
+            fontProps={{ size: HEADER_SIZE, color: "#f0fff0" }}
           />
         </div>
       </div>
       <div className={styles.mainPage}>
         {isMemu ? (
           <div className={styles.menuContainer}>
-            <PixelWriting str={"Hello World!"} fontProps={{ size: 4 }} />
+            <PixelWriting
+              str={"Hello World!"}
+              fontProps={{ size: FONT_SIZE }}
+            />
           </div>
         ) : (
           <div className={styles.contentContainer}>
-            <PixelWriting str={"My name is you."} fontProps={{ size: 4 }} />
+            {content === "main" ? (
+              <div className={styles.menuContainer}>
+                <div
+                  onClick={() => {
+                    playMusic();
+                  }}
+                  style={{ cursor: "pointer" }}
+                >
+                  <PixelWriting
+                    str={"Hello Main!"}
+                    fontProps={{ size: FONT_SIZE }}
+                  />
+                </div>
+              </div>
+            ) : (
+              <div className={styles.contentContainer}>
+                <div
+                  onClick={() => {
+                    playMusic();
+                  }}
+                  style={{ cursor: "pointer" }}
+                >
+                  <PixelWriting
+                    str={"Hello World!"}
+                    fontProps={{ size: FONT_SIZE }}
+                  />
+                </div>
+              </div>
+            )}
           </div>
         )}
       </div>
       <div className={styles.footer}>
-        <PixelWriting str={"Bye World!"} fontProps={{ size: 4 }} />
+        <div
+          onClick={() => {
+            setIsMemu(!isMemu);
+          }}
+          style={{ cursor: "pointer" }}
+        >
+          <PixelWriting str={"github"} fontProps={{ size: FONT_SIZE }} />
+        </div>
       </div>
     </div>
   );
